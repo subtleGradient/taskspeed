b93cedecbf725359ada668a88c3db0159b78a00e
John Resig <jeresig@gmail.com>
2009-02-27 13:31:26 -0500

    Updated the jQuery tests to match what's listed in the sample file and better use jQuery techniques.

--- tests/jq-tests.js
+++ tests/jq-tests.js
@@ -1,19 +1,18 @@
 window.tests = {
 	
 	"make": function(){
+		var str = "";
 		for(var i = 0; i<250; i++){
-			$("<ul id='setid" + i + "' class='fromcode'></ul>")
-				.appendTo("body")
-				.append("<li>one</li><li>two</li><li>three</li>");
+			str += "<ul id='setid" + i + "' class='fromcode'><li>one</li><li>two</li><li>three</li></ul>";
 		}
+		$("body").append(str);
 		return $("ul.fromcode").length;
 	},
 	
 	"indexof" : function(){
 		var n, id;
 		for(var i = 0; i < 20; i++){
-			id = $("#setid150");
-			n = $("ul").index(id)
+			n = $("ul").index( $("#setid150")[0] )
 		}
 		return n;
 	},
@@ -23,96 +22,73 @@ window.tests = {
 	},
 	
 	"attr" : function(){
-		return $("ul").map(function(){ return $(this).attr("id") }).length;
+		return $("ul").map(function(){ return this.id; }).length;
 	},
 	
 	"bindattr" : function(){
+		function someFn(){}
 		return $("ul > li")
-			.bind("mouseover", function(){ })
-			.attr("rel", "jq")
+			.bind("mouseover", someFn)
+			.attr("rel", "touched")
 			.unbind("mouseover")
 			.length;
+	},
+
+	"table": function(){
+		var str = "";
+		for(var i = 0; i < 40; i++){
+			str += "<table class='madetable'><tr><td>first</td></tr></table>";
+		}
+
+		$("body").append(str);
+		$(".madetable tr").prepend("<td>before</td>");
+		return $("td").length;
 	},
 	
 	"addanchor" : function(){
 		return $(".fromcode > li").append("<a href='http://example.com'>link</a>").length;
 	},
 	
-	"alt-add" : function(){
-		return $(".fromcode > li").each(function(){
-			$("<a href='http://example.com'>link2</a>").appendTo(this);
-		}).length;
-	},
-	
-	"create": function(){
+	"append": function(){
+		var str = "";
 		for(var i = 0; i<500; i++){
-			$("body").append("<div rel='foo'>test</div>");
+			str += "<div rel='foo'>test</div>";
 		}
+		$("body").append(str);
 		return $("[rel^='foo']").length;
-	},
-	
-	"append" : function(){
-		for(var i = 0; i<500; i++){
-			$("<div rel='foo2'>test2</div>").appendTo("body");
-		}
-		return $("div[rel^='foo2']").length;
 	},
 	
 	"addclass-odd" : function(){
 		return $("div").addClass("added").filter(":odd").addClass("odd").length;
 	},
 	
-	
 	"style" : function(){
 		return $(".added").css({ backgroundColor:"#ededed", color:"#fff" }).length;
 	},
 
-	"confirm-added" : function(){
-		return $("div.added").length;
-	},
-	
 	"removeclass" : function(){
 		return $(".added").removeClass("added").length;
 	},
 	
-	"table": function(){
-		for(var i = 0; i < 40; i++){
-			$("<table class='madetable'></table>")
-				.appendTo("body")
-				.html("<tr><td>first</td></tr>")
-				.find("tr").prepend("<td>before</td>");
-		}
-		return $("tr td").length;
-	},
-	
 	"sethtml": function(){
-		return $("div").each(function(){
-			this.innerHTML = "<p>jquery's new content</p>";
-		}).length;	
-	},
-	
-	"sethtml-alt" : function(){
-		// only reducing this because .html can't handle 5000 either
-		return $(".odd").map(function(i){
-			return i % 50 === 0 ? this : null;
-		}).length;
+		return $("div").html("<p>new content</p>").length;
 	},
 	
 	"insertbefore" : function(){
-		return $(".fromcode a").before($("<p>A Link</p>")).length;
+		return $(".fromcode a").before("<p>A Link</p>").length;
 	},
 	
 	"insertafter" : function(){
-		return $(".fromcode a").after($("<p>After Link</p>")).length;
+		return $(".fromcode a").after("<p>After Link</p>").length;
 	},
 	
-	destroy: function(){
+	"destroy": function(){
 		return $(".fromcode").remove().length;
 	},
 	
-	finale: function(){
+	"finale": function(){
 		$("body").empty();
 		return $("body *").length;
 	}
 	
-}
+};
