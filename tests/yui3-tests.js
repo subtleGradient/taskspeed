bac7734adef5f13d11b787e62ab56a6283036b9f
Eric Ferraiuolo <eferraiuolo@gmail.com>
2009-04-01 02:40:23 -0400

    added a first run at YUI3 tests, still have a few more to do

---
+++ tests/yui3-tests.js
@@ -0,0 +1,138 @@
+
+window.tests = {
+	
+	"make" : function(){	
+		var ul, i;
+		for ( i = 0; i < 250; i++ ) {
+			ul = Y.Node.create('<ul></ul>');
+			ul.addClass('fromcode');
+			ul.set('id', 'setid'+i).set('innerHTML', '<li>one</li><li>two</li><li>three</li>');
+			Y.get('body').appendChild(ul);
+		}
+		return Y.all('ul.fromcode').size();
+	},
+	
+	"indexof" : function(){
+		var target, item, index, i;
+		for ( i = 0; i < 20; i++ ) {
+			target = Y.get('#setid150');
+			item = 0;
+			index = 0;
+			Y.all('ul').each(function(n){
+				if ( n === target ) {
+					index = item;
+				}
+				item ++;
+			});
+		}
+		return index;
+	},
+	
+	"bind" : function(){
+		var nodes = Y.all('ul > li');
+		nodes.on('click', function(){});
+		return nodes.size();
+	},
+	
+	"attr" : function(){
+		return Y.all('ul').get('id').length;
+	},
+	
+	"bindattr" : function(){
+		var nodes = Y.all('ul > li');
+		nodes.on('mouseover', function(){});
+		nodes.set('rel', 'touched');
+		nodes.detach('mouseover');
+		return nodes.size();
+	},
+
+	"table": function(){
+		// in a 40-iteration for loop:
+		//		create a table with the class "madetable", and append it to the DOM
+		//		add a row with one cell to the table. the cell content should be "first"
+		//		add a new cell before the first cell in the row.
+		//
+		//	return the length of the query "tr td"
+		var table, i;
+		for ( i = 0; i < 40; i++ ) {
+			table = Y.Node.create('<table class="madetable"></table>');
+			Y.get('body').appendChild(table);
+			table.set('innerHTML', '<tr><td>first</td></tr>');
+			table.insertBefore( Y.Node.create('<td></td>'), table.query('td') );
+		}
+		return Y.all('tr td').size();
+	},
+	
+	"addanchor" : function(){
+		var lis = Y.all('.fromcode > li');
+		lis.set('innerHTML', '<a href="http://example.com">link</a>')
+		return lis.size();
+	},
+
+	"append" : function(){
+		//	in a 500 iteration loop:
+		//		create a new <div> with the same critera as 'create'
+		//			- NOTE: rel needs to be == "foo2"
+		//		then append to body element (no caching)
+		//		
+		//	return then length of the matches from the selector "div[rel^='foo2']" 
+		for ( var i = 0; i < 500; i++ ) {
+			Y.get('body').appendChild(Y.Node.create('<div rel="foo2"></div>'));
+		}
+		return Y.all("div[rel^='foo2']").size();
+	},
+	
+	"addclass-odd" : function(){
+		// locate all div elements on the page
+		//	add the class "added" to those divs
+		//	add the class "odd" to the odd divs in the selection
+		//
+		// return the lenght of the odd found divs
+		var divs = Y.all('div');
+		divs.addClass('added');
+		return divs.filter(':nth-child(2n+1)').addClass('odd').length;
+	},
+	
+	"style": function(){
+		return Y.all('.added').setStyles({ 'background-color':'#ededed', 'color':'#fff' }).length;
+	},
+	
+	"removeclass": function(){
+		return Y.all('.added').removeClass('added').length;
+	},
+	
+	"sethtml": function(){
+		// replace the content of all div elements with "<p>new content</p>"
+		//
+		// return the length of matched divs
+		return Y.all('div').set('innerHTML', '<p>new content</p>').length;
+	},
+	
+	"insertbefore" : function(){
+		//	find all anchors in the class "fromcode" (.fromcode a)
+		//		add a <p> element in the dom before the matched anchors
+		//			- the content should equal "A Link"
+		//		
+		//	return the length of the found anchors.
+	},
+	
+	"insertafter" : function(){
+		//	find all anchors in the class "fromcode" (.fromcode a)
+		//		add a <p> element in the dom after the matched anchors
+		//			- the content should equial "After Link"
+		//			
+		//	return the length of the found anchors.
+	},
+	
+	"destroy": function(){ 
+		// destroy all the nodes with the class "fromcode"
+		// return the length of the destroyed nodes
+	},
+	
+	"finale": function(){
+		// empty the body element of all elements
+		//
+		// return the length of the query "body *"
+	}
+	
+};
